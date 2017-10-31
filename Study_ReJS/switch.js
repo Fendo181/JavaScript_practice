db223187dfaf5d39505216019cf1823138cf348b
Fendo181 <fendo181git@gmail.com>
2017-10-31 12:13:21 +0900

    例外処理に関する処理を追加した。

---
+++ Study_ReJS/switch.js
@@ -0,0 +1,12 @@
+var flutes_name = "Banana";
+
+switch (flutes_name) {
+    case "Orange":
+        console.log("これはオレンジの匂いがするぜ!")
+        break;
+    case "Banana":
+        console.log("これはバナナの匂いですね！")
+        break;
+    default:
+        break;
+}
