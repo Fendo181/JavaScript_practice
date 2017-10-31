4740396ba68744bf04cf0143565202995f776fa7
endu <futoshi.endo@pepabo.com>
2017-10-31 10:42:50 +0900

    add Array.js

---
+++ Study_ReJS/array.js
@@ -0,0 +1,9 @@
+var coffes = ["FrenchRoast", "Colombian", "Kona"];
+coffes.forEach(function (item,index, array) {
+    console.log(item, index);
+});
+
+var fruits = ["りんご", "バナナ"];
+fruits.forEach(function (item, index, array) {
+  console.log(item, index);
+});
