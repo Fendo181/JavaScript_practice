e2f9338a8f037a7dcb10a4dcd501382802fa290e
Fendo181 <fendo181git@gmail.com>
2017-11-01 19:27:43 +0900

    ipdate auto_paging

--- Study_jQuery/basic/suzuri_auto_paging.js
+++ Study_jQuery/basic/suzuri_auto_paging.js
@@ -12,13 +12,14 @@
 
 
 $(function() {
-    var SuzuriContentItems = '.ncgr-section __inner ncgr-section--gray'; // 取得する要素
-    var SuzuriContent = '.ncgr-card ncgr-mar-b-24'; // 取得要素を追加するコンテンツ
+    var SuzuriContentItems = '.ncgr-section.ncgr-section--gray .ncgr-section__inner.ncgr-section__inner--narrow .ncgr-card.ncgr-mar-b-24'; // 取得する要素
+    var SuzuriContent = '.ncgr-section__inner.ncgr-section__inner--narrow'; // 取得要素を追加するコンテンツ
     var SuzuriNextLink = '.journal-pagenation .pagination .next a'; // 次のページのリンク
     var SuzuriNextLinkArea = '.journal-pagenation'; // 次のページのリンクの親要素
+    var SuzuriNextLinkList = '.journal-pagenation .pagination';
 
     var loadingFlag = false; // 読み込み中はtrueにして、複数回発生しないようにする
-    let base_url ="https://suzuri.jp/";
+    let  base_url      ="https://suzuri.jp/";
 
     // スクロール開示に読み込みが発生する。
     $(window).on('load scroll', function() {
@@ -36,27 +37,31 @@ $(function() {
                 // 次のページのリンクを取得して、要素を削除しておく
                 var nextPage = base_url+$(SuzuriNextLink).attr('href');
                console.log("次のリンクは"+nextPage);
-                $(SuzuriNextLink).remove();
+                $('.journal-pagenation').remove();
                 // 次のページの要素を取得
                 $.ajax({
                     type: 'GET',
                     url: nextPage,
                     dataType: 'html'
                 }).done(function(data) {
+          　　　　　　　　　 console.log("通信に成功しました！");
                     // 次のページのリンクを取得
-                    var nextLink = $(data).find(SuzuriNextLink);
-                     console.log(nextLink);
+                    var nextLink =  base_url + $(data).find(SuzuriNextLink).attr('href');
+                     console.log("次のリンクは"+nextLink);
                     // コンテンツ要素を取得
                     var contentItems = $(data).find(SuzuriContentItems);
-                    console.log(contentItems);
                     // コンテンツ要素を追加
                     $(SuzuriContent).append(contentItems);
                     // 次のページがある場合はリンクを追加する
                     if(nextLink.length > 0) {
+                        console.log("次についかされるリンクは"+nextLink);
+
                         $(SuzuriNextLinkArea).append(nextLink);
+                        console.log("次のリンクは"+nextLink);
                         loadingFlag = false; // 次のページがない場合はloadingFlagをtrueにしたままにして、処理を発生しないようにする
                     }
                 }).fail(function () {
+                    console.log("通信に失敗しました！！");
                     alert('ページの取得に失敗しました。');
                 });
             }
