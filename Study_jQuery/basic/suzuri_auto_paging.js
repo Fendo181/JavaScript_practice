408013081bf7b1de25dc15f60c81005e5c9919c1
endu <futoshi.endo@pepabo.com>
2017-11-02 11:00:00 +0900

    surosuroくんのジャーナルだけで動作するauto pagerを作成した。

--- Study_jQuery/basic/suzuri_auto_paging.js
+++ Study_jQuery/basic/suzuri_auto_paging.js
@@ -16,7 +16,6 @@ $(function() {
     var SuzuriContent = '.ncgr-section__inner.ncgr-section__inner--narrow'; // 取得要素を追加するコンテンツ
     var SuzuriNextLink = '.journal-pagenation .pagination .next a'; // 次のページのリンク
     var SuzuriNextLinkArea = '.journal-pagenation'; // 次のページのリンクの親要素
-    var SuzuriNextLinkList = '.journal-pagenation .pagination';
 
     var loadingFlag = false; // 読み込み中はtrueにして、複数回発生しないようにする
     let  base_url      ="https://suzuri.jp/";
@@ -25,40 +24,35 @@ $(function() {
     $(window).on('load scroll', function() {
         if(!loadingFlag) {
             var winHeight = $(window).height();
-            console.log("windowの高さ"+winHeight);
             var scrollPos = $(window).scrollTop();
-            console.log("スクロールの位置"+scrollPos);
             var linkPos = $(SuzuriNextLink).height();
-            console.log("nextリンクの高さ"+linkPos);
 
-            // より上にある場合は
             if(winHeight + scrollPos > linkPos) {
                 loadingFlag = true;
+               var nextPage = base_url+$(SuzuriNextLink).attr('href');
+
                 // 次のページのリンクを取得して、要素を削除しておく
-                var nextPage = base_url+$(SuzuriNextLink).attr('href');
                console.log("次のリンクは"+nextPage);
-                $('.journal-pagenation').remove();
-                // 次のページの要素を取得
+                $(SuzuriNextLinkArea).remove();
+
                 $.ajax({
                     type: 'GET',
                     url: nextPage,
                     dataType: 'html'
                 }).done(function(data) {
-          　　　　　　　　　 console.log("通信に成功しました！");
-                    // 次のページのリンクを取得
-                    var nextLink =  base_url + $(data).find(SuzuriNextLink).attr('href');
-                     console.log("次のリンクは"+nextLink);
+                     console.log("通信に成功しました！");
                     // コンテンツ要素を取得
-                    var contentItems = $(data).find(SuzuriContentItems);
+                    var contentItems = $(data).find(SuzuriContentItems);  //記事
+                    var journal_page =  $(data).find(SuzuriNextLinkArea); //リンクエリア
+
                     // コンテンツ要素を追加
                     $(SuzuriContent).append(contentItems);
-                    // 次のページがある場合はリンクを追加する
-                    if(nextLink.length > 0) {
-                        console.log("次についかされるリンクは"+nextLink);
+                    $(SuzuriContent).append(journal_page);
 
-                        $(SuzuriNextLinkArea).append(nextLink);
-                        console.log("次のリンクは"+nextLink);
-                        loadingFlag = false; // 次のページがない場合はloadingFlagをtrueにしたままにして、処理を発生しないようにする
+                    // 次のページがある場合はリンクを追加する
+                    if(journal_page.length) {
+                        $(SuzuriContent).append(journal_page);
+                        loadingFlag = false; //読み込みが成功したら、loadingFlagをfalsにして再度読み込みを開始させる。　
                     }
                 }).fail(function () {
                     console.log("通信に失敗しました！！");
