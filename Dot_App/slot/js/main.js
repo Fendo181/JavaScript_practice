41c4d6853c2ab936ac4dea1ef92c6604ba88e7ca
Fendo181 <fendo181git@gmail.com>
2017-11-04 23:52:57 +0000

    Add SlotApp

---
+++ Dot_App/slot/js/main.js
@@ -0,0 +1,86 @@
+(function () {
+    'use strict';
+
+    var panels = document.getElementsByClassName('panel');
+    var spin = document.getElementById('spin');
+    var spinspeed = 45;
+
+    // img array
+    var slot_card = [
+        'seven.png',
+        'bell.png',
+        'cherry.png'
+    ];
+    
+    var timers = [];
+
+    var stopCount = 0;
+
+    function runSlot(n) {
+        timers[n] = setTimeout(function() {
+            panels[n].children[0].src = 
+            'img/' +  
+            slot_card[Math.floor(Math.random() * slot_card.length)];
+            // console.log(n)
+            // 再帰
+            runSlot(n)
+        }, spinspeed);
+    }
+
+    function stopPanel(params) {
+        var i;
+        for (i =0; i < panels.length; i++){
+            panels[i].children[1].addEventListener('click', 
+            function(){
+                // ata-index number
+                console.log(this.dataset.index)
+                if (this.className.indexOf('inactive') !== -1) {
+                    // 処理が止まる
+                    return;
+                }
+                clearTimeout(timers[this.dataset.index]);
+                stopCount++;
+                this.className = 'stop inactive';
+                if (stopCount === panels.length) {
+                    stopCount = 0;
+                    checkResults();
+                    spin.className = '';
+                }
+            });
+        }
+    }
+
+    function checkResults(){
+        var img0 = panels[0].children[0];
+        var img1 = panels[1].children[0];
+        var img2 = panels[2].children[0];
+
+        if (img0.src !== img1.src && img0.src !== img2.src) {
+            img0.className = 'unmatched';
+        }
+        if (img1.src !== img0.src && img1.src !== img2.src) {
+            img1.className = 'unmatched';
+        }
+        if (img2.src !== img0.src && img2.src !== img1.src) {
+            img2.className = 'unmatched';
+        }
+
+    }
+
+    stopPanel()
+
+    spin.addEventListener('click',function () {
+        var i;
+        // inactiveが含まれていたら
+        if (this.className.indexOf('inactive') !== -1) {
+            // 処理が止まる
+            return;
+        }
+        this.className = 'inactive';
+        for (i = 0; i < panels.length; i++){
+            runSlot(i)
+            panels[i].children[0].className = '';
+            panels[i].children[1].className = 'stop';
+        }
+    });
+})();
