775c4108b66a000d2434819447830ab6898de07d
Fendo181 <fendo181git@gmail.com>
2017-11-13 15:34:21 +0900

    update userScripts

--- UserScripts/suzuri_web/add_ncgr.js
+++ UserScripts/suzuri_web/add_ncgr.js
@@ -11,15 +11,14 @@
 (function (){
     
     'user strict';
-    
-        var categoriNavi = document.querySelectorAll('.js-ncgr-category-navigation__list-item');
+        var categoryNavi = document.querySelectorAll('.js-ncgr-category-navigation__list-item');
         for(var i=0; i<categoriNavi.length; i++) {
 
             categoriNavi[i].addEventListener("mouseover",function(){
                 var categoriMenu = this.querySelector('.js-ncgr-category-menu-'+this.dataset.category);
                 categoriMenu .classList.add('ncgr-is-visible');
             });
-            
+
             categoriNavi[i].addEventListener("mouseout",function(){
                 var categoriMenu = this.querySelector('.js-ncgr-category-menu-'+this.dataset.category);
                 categoriMenu.classList.remove('ncgr-is-visible');
