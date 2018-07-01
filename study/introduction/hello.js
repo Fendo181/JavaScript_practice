d9411c8d553f5503dc6b347ab0d12732104cdbba
Fendo181 <fendo181git@gmail.com>
2018-07-01 22:35:50 +0900

    p53 テンプレート文字列
    文字列への変数の埋め込み
    複数行にまたがる文字列の生成

--- study/introduction/hello.js
+++ study/introduction/hello.js
@@ -1,12 +1,4 @@
-console.log('Hello Wold!');
+let message = "hello wold!";
+let str = `こんにちは！　${message}`;
 
-const TAX = 1.08;
-// const TAX = 1.12;
-let price = 100;
-console.log(price*TAX);
-
-
-/*
-constも変数の重複ができない
-hello.js:4 Uncaught SyntaxError: Identifier 'TAX' has already been declared
-*/
+console.log(str) //こんにちは！　hello wold!
