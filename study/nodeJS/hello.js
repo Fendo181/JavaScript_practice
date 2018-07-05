093e9857f5a99bd657d65820e22deafa99dbd8e7
Fendo181 <fendo181git@gmail.com>
2018-07-05 09:07:23 +0900

    non-blocking処理

--- study/nodeJS/hello.js
+++ study/nodeJS/hello.js
@@ -1,1 +1,10 @@
-console.log('hello js!'); //hello js!
+// console.log('hello js!'); //hello js!
+
+// non blockingな書き方
+// callバック関数
+
+// 先にwoldが先に出力される
+setTimeout(function() {
+    console.log('Hello');
+}, 1000);
+console.log('wold')
