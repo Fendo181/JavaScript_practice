e88af244c27d6a05e27d95952a71467345878d1b
Fendo181 <fendo181git@gmail.com>
2018-07-05 22:55:03 +0900

    #7 URLによって処理を変えてみる。
     req.url でアクセスしてきた URL を取得して、それによって処理を振り分けることもできると覚えておいてください。

--- study/nodeJS/server.js
+++ study/nodeJS/server.js
@@ -4,12 +4,24 @@ let server = http.createServer();
 let setttings = require('./settings');
 
 console.log(setttings);
+let msg;
 
 server.on('request',function(req, res) {
+    switch(req.url) {
+        case '/about':
+            msg = "about this pages";
+            break;
+        case '/profile':
+            msg = 'about me';
+            break;
+        default:
+            msg = 'wrong page!'
+            break;
+    }
     // onをつけるとイベントを繋げられる
     res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.write('hello wold!');
-    //
+
+    res.write(msg);
     res.end();
 });
 
