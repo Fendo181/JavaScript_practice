16fe475d79dc1b327653e6818b8316d7aef12c1f
Fendo181 <fendo181git@gmail.com>
2018-07-05 09:10:37 +0900

    blocking処理
    時間がかかりそうな処理はコールバック関数で実装する

--- study/nodeJS/hello.js
+++ study/nodeJS/hello.js
@@ -4,7 +4,15 @@
 // callバック関数
 
 // 先にwoldが先に出力される
-setTimeout(function() {
-    console.log('Hello');
-}, 1000);
-console.log('wold')
+// setTimeout(function() {
+//     console.log('Hello');
+// }, 1000);
+// console.log('wold')
+
+// blocking
+
+// while文が終わるまで次の処理止まってしまう
+
+let start = new Date().getTime();
+while (new Date().getTime() < start + 1000);
+console.log('world!');
