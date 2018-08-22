40b4375084e23f55b66999bfc2b7dfd45637ec16
Fendo181 <fendo181git@gmail.com>
2018-08-22 09:39:53 +0900

    #02 Vue.jsの基本を押させる
    双方向バインディングを使って、ViewModelからUIにデータを反映させる

--- vue/basic_v_2.5.13/js/mian.js
+++ vue/basic_v_2.5.13/js/mian.js
@@ -1,4 +1,13 @@
 (function(){
     'use strict';
 
-})
+    // two way data binding (to UI)
+
+    // UI に結びつく View Modelを作る
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            name: 'endo!'
+        }
+    });
+})();
