f56f744e282f9fe4798f006d1059ee4827b79236
Fendo181 <fendo181git@gmail.com>
2018-10-23 10:29:49 +0900

    15 いいね!ボタンを実装する
    Vue.extendでコンポーネント内にdataとメソッドを定義する
    
    むっずい

--- vue/basic_v_2.5.13/main.js
+++ vue/basic_v_2.5.13/main.js
@@ -4,14 +4,26 @@
     // Component
     // テンプレートを作成する
     let likeComponent = Vue.extend({
-        template: '<button>Likeだよ!</button>'
+        // Component の data は関数で返してあげる必要がある
+        data: function() {
+            return {
+                count: 0
+            }
+        },
+        methods:{
+            countUp: function() {
+                this.count++;
+            }
+        },
+        // htmlタグ
+        template: '<button @click="countUp">Like {{ count }}</button>',
     });
 
     let app = new Vue({
     el: '#app',
         components: {
-            // コンポーネントを指定する
-            'like-component': likeComponent
+        // コンポーネントを指定する
+        'like-component': likeComponent
         },
     });
 
