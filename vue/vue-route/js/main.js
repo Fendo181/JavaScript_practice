722d2c127621326eaa54f70190b56af9969aae70
Fendo181 <fendo181git@gmail.com>
2018-11-06 00:18:58 +0900

    vue-jsのコンポーネントを作成してHTMLで表示するようにした

---
+++ vue/vue-route/js/main.js
@@ -0,0 +1,23 @@
+// 1 コンポーネントを定義する
+const Foo = {template: '<div>foo</div>' };
+const Bar = {template: '<div>Bar</div>' };
+
+// 2 ルートをいくつか定義します
+// 各ルートは1つのコンポーネントとマッピングされる必要があります。
+// このコンポーネントは実装のVue.extend()、
+// またはコンポーネントオプションのオブジェクトでも構いません
+
+const routes = [
+    { path: '/foo', comment: Foo },
+    { path: '/bar', comment: Bar },
+];
+
+// 3.ルータインスタンスを作成して、ルートオプションを渡します。
+const router = new VueRouter({
+    routes //routes: routesの短縮表記
+});
+
+// 4. rootとなるインスタンスを生成して、マウントします。
+const app = new Vue({
+    router
+}).$mount('#app');
