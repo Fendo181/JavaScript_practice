4a40b524c9e59470668686780f0b65d9ca50c26b
Fendo181 <fendo181git@gmail.com>
2018-11-12 23:43:54 +0900

    p66 「...」演算子を使えれば、分解されなかった変数が入る

--- introduction/chap2/dest.js
+++ introduction/chap2/dest.js
@@ -1,9 +1,10 @@
 let data = [10,20,30,40,50,50,60];
-let [x1,x2,x3,x4,x5,x6] = data;
+let [x1,x2, ...other] = data;
 
 console.log(x1);
 console.log(x2);
-console.log(x3);
-console.log(x4);
-console.log(x5);
+console.log(other);
 
+// 10
+// 20
+// [ 30, 40, 50, 50, 60 ]
