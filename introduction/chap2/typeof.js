6a5c93e6262b4418e71ccff52a6f343f8530f0ca
Fendo181 <fendo181git@gmail.com>
2018-11-14 01:05:56 +0900

    typeof演算子

---
+++ introduction/chap2/typeof.js
@@ -0,0 +1,15 @@
+let num =1;
+console.log(typeof num); //number
+
+let str ='endo';
+console.log(typeof str); //string
+
+let flg =true;
+console.log(typeof flg); //boolean
+
+let obj = {x:1,x:2};
+console.log(typeof obj); //bject
+
+let ary = ['endl','takahashi'];
+console.log(typeof obj); //bject
+
