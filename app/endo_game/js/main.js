7e5bf6a78d6bf8eda5e61391d46b9c7c142bfa4e
Fendo181 <fendo181git@gmail.com>
2018-12-07 08:50:30 +0900

    簡単な動くサンプルアプリケーションを作成
    ref: http://runstant.com/phi/projects/phinajs_examples_walktomapiko

--- app/endo_game/js/main.js
+++ app/endo_game/js/main.js
@@ -1,27 +1,86 @@
 // phina.js をグローバル領域に展開
 phina.globalize();
 
+// 定数
+const ASSETS = {
+  image: {
+    bg: "http://jsrun.it/assets/a/G/5/Y/aG5YD.png",
+    tomapiko: 'http://cdn.rawgit.com/phi-jp/phina.js/v0.2.0/assets/images/tomapiko_ss.png',
+  },
+};
+const SCREEN_WIDTH = 465; // スクリーン幅
+const SCREEN_HEIGHT = 465; // スクリーン高さ
+const SPEED = 4;
+
 // MainScene クラスを定義
 phina.define('MainScene', {
   superClass: 'DisplayScene',
-  init: function() {
-    this.superInit();
+  // 初期化
+  init: function (options) {
+    this.superInit(options);
     // 背景色を指定
-    this.backgroundColor = '#444';
-    // ラベルを生成
-    this.label = Label('Hello, phina.js!').addChildTo(this);
-    this.label.x = this.gridX.center(); // x 座標
-    this.label.y = this.gridY.center(); // y 座標
-    this.label.fill = 'white'; // 塗りつぶし色
+    // this.backgroundColor = '#444';
+    // 背景
+    this.bg = Sprite("bg").addChildTo(this);
+    this.bg.origin.set(0, 0); // 左上基準に変更
+
+    // // ラベルを生成
+    // this.label = Label('Endo Game!').addChildTo(this);
+    // this.label.x = this.gridX.center(); // x 座標
+    // this.label.y = this.gridY.center(); // y 座標
+    // this.label.fill = 'white'; // 塗りつぶし色
+
+    //プレイヤーを生成する
+    this.player = Sprite('tomapiko', 64, 64).addChildTo(this);
+    this.player.setPosition(400, 400);
+    this.player.frameIndex = 0;
   },
+
+  // 更新
+  update: function (app) {
+    var p = app.pointer;
+
+    if (p.getPointing()) {
+      var diff = this.player.x - p.x;
+      if (Math.abs(diff) > SPEED) {
+        // 右に移動
+        if (diff < 0) {
+          this.player.x += SPEED;
+          this.player.scaleX = -1;
+        }
+        // 左に移動
+        else {
+          this.player.x -= SPEED;
+          this.player.scaleX = 1;
+        }
+
+        // フレームアニメーション
+        if (app.frame % 4 === 0) {
+          this.player.frameIndex = (this.player.frameIndex === 12) ? 13 : 12;
+        }
+      }
+    } else {
+      // 待機
+      this.player.frameIndex = 0;
+    }
+  }
 });
 
+//更新処理
+
+
+
+
 // メイン処理
-phina.main(function() {
+phina.main(function () {
   // アプリケーション生成
   var app = GameApp({
     startLabel: 'main', // メインシーンから開始する
+    width: SCREEN_WIDTH,  // 画面幅
+    height: SCREEN_HEIGHT,// 画面高さ
+    assets: ASSETS,       // アセット読み込
   });
+  app.enableStats();
   // アプリケーション実行
   app.run();
 });
