b4403c67a6728dec8730dcae6b52b06ef9ca058e
Fendo181 <fendo181git@gmail.com>
2018-12-14 09:50:40 +0900

    g オプションを外すとグローバル検索をやめて一致したタイミングで正規表現をマッチさせる

--- introduction/chap3/matc.js
+++ introduction/chap3/matc.js
@@ -1,9 +1,9 @@
-var p = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/gi;
+var p = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/i;
 var str = 'サポートサイトはhttp://wwww.wings.msn.to/です！';
 str += 'サンプル紹介サイトはhttp://www.web-deli-.com';
 var result = str.match(p);
 console.log(result);
-console.log(result[1]);
+// console.log(result[1]);
 
 for(var i = 0, len=result.length; i<len; i++){
     console.log(result[i]);
