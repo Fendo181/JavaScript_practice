f13bc364ff3fb4b589e2438748e2bc0cb78360b7
Fendo181 <fendo181git@gmail.com>
2019-01-06 08:51:29 +0900

    js内で当たりの判定を行うようにした

--- study/first/js/main.js
+++ study/first/js/main.js
@@ -1,11 +1,26 @@
 "use strict";
 
-for (let i = 0; i < 10; i++) {
+const num = 5;
+// 0~5以上のランダムな数字を出す、
+const winner = Math.floor(Math.random()*num);
+
+for (let i = 0; i < num; i++) {
     const div = document.createElement('div');
     div.classList.add('box');
-    div.textContent = i;
+    if( i === winner ){
+        div.dataset.result = 'win';
+    }else {
+        div.dataset.result = 'lose';
+    }
+
     div.addEventListener('click', function () {
-        div.classList.toggle('circle');
+        if( div.dataset.result === 'win' ){
+            div.textContent = 'あたりです！'
+            div.classList.add('win');
+        }else {
+            div.textContent = 'ハズレです'
+            div.classList.add('lose');
+        }
     });
     // bodyの小要素として追加する
     document.body.appendChild(div);
