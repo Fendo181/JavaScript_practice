e603ebcfc64c508a93f76323fa2f958e647bd2e2
Fendo181 <fendo181git@gmail.com>
2019-01-06 08:28:48 +0900

    クリックしたらアニメーションで回すようにした

--- study/first/js/main.js
+++ study/first/js/main.js
@@ -2,5 +2,5 @@
 let tartget = document.getElementById('target');
 // クリックしたときに動作させる
 tartget.addEventListener('click' , function(){
-    tartget.style.background = 'pink';
+    tartget.classList.add('circle');
 });
