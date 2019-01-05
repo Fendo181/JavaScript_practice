bec429003ae19ff3b378e62d0661f90a523d8168
Fendo181 <fendo181git@gmail.com>
2019-01-06 08:41:45 +0900

    jsで動的にdivが生成されるようにした

--- study/first/js/main.js
+++ study/first/js/main.js
@@ -1,19 +1,11 @@
 "use strict";
-let tartget1 = document.getElementById('target1');
-let tartget2 = document.getElementById('target2');
-let tartget3 = document.getElementById('target3');
-let tartget4 = document.getElementById('target4');
 
-// クリックしたときに動作させる
-tartget1.addEventListener('click' , function(){
-    tartget1.classList.toggle('circle');
+const div = document.createElement('div');
+
+div.classList.add('box');
+div.addEventListener('click', function(){
+    div.classList.toggle('circle');
 });
-tartget2.addEventListener('click' , function(){
-    tartget2.classList.toggle('circle');
-});
-tartget3.addEventListener('click' , function(){
-    tartget3.classList.toggle('circle');
-});
-tartget4.addEventListener('click' , function(){
-    tartget4.classList.toggle('circle');
-});
+
+// bodyの小要素として追加する
+document.body.appendChild(div);
