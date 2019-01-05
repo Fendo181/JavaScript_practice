4fa385869bc46787007fb621ff2d711e0d1fbd77
Fendo181 <fendo181git@gmail.com>
2019-01-06 08:38:13 +0900

    複数のboxを作るようにした

--- study/first/js/main.js
+++ study/first/js/main.js
@@ -1,6 +1,19 @@
 "use strict";
-let tartget = document.getElementById('target');
+let tartget1 = document.getElementById('target1');
+let tartget2 = document.getElementById('target2');
+let tartget3 = document.getElementById('target3');
+let tartget4 = document.getElementById('target4');
+
 // クリックしたときに動作させる
-tartget.addEventListener('click' , function(){
-    tartget.classList.toggle('circle');
+tartget1.addEventListener('click' , function(){
+    tartget1.classList.toggle('circle');
+});
+tartget2.addEventListener('click' , function(){
+    tartget2.classList.toggle('circle');
+});
+tartget3.addEventListener('click' , function(){
+    tartget3.classList.toggle('circle');
+});
+tartget4.addEventListener('click' , function(){
+    tartget4.classList.toggle('circle');
 });
