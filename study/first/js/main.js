20648edccad91b7436812e9f51d606ff0bf3ca9f
Fendo181 <fendo181git@gmail.com>
2019-01-06 08:44:04 +0900

    for文を使って動的に生成するようにした

--- study/first/js/main.js
+++ study/first/js/main.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const div = document.createElement('div');
-
-div.classList.add('box');
-div.addEventListener('click', function(){
-    div.classList.toggle('circle');
-});
-
-// bodyの小要素として追加する
-document.body.appendChild(div);
+for (let i = 0; i < 10; i++) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.addEventListener('click', function () {
+        div.classList.toggle('circle');
+    });
+    // bodyの小要素として追加する
+    document.body.appendChild(div);
+}
