52b92d84b68103e1ec59700343d430cb002ae7cb
Fendo181 <fendo181git@gmail.com>
2019-01-08 09:42:16 +0900

    引数が1つの場合は、されらに引数を囲む()を省略できる

--- introduction/chap4/arrow.js
+++ introduction/chap4/arrow.js
@@ -1,2 +1,2 @@
-let getTriangle = (base,height) => (base*height)/2;
-console.log('三角形の面積は'+getTriangle(10,20)); //三角形の面積は100
+let getTriangle = base => (base*base)/2;
+console.log('三角形の面積は'+getTriangle(10)); //三角形の面積は50
