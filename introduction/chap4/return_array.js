1bde81c80958887f9d94770a2828350094b1fec8
Fendo181 <fendo181git@gmail.com>
2019-01-23 09:42:21 +0900

    maxだけ切り捨てる事もできる

--- introduction/chap4/return_array.js
+++ introduction/chap4/return_array.js
@@ -5,11 +5,7 @@ function getMaxMin(...nums){
 let result = getMaxMin(10,20,30,40,50);
 console.log(result);
 
-let [max,min] = getMaxMin(-10,15,32,25,42);
-console.log(max);
+let [,min] = getMaxMin(-10,15,32,25,42);
+// console.log(max);
 console.log(min);
-
-// [ 50, 10 ]
-// 42
-// -10
 
