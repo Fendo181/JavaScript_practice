96a038858e8745f6a7abb49e9f83f3d7f22ed884
Fendo181 <fendo181git@gmail.com>
2019-01-24 10:04:13 +0900

    p214 関数の名前を定義しなくても単純に匿名関数として引数で呼び出す事も可能
    でもわかりづらいなぁ..

---
+++ introduction/chap4/higher_anonymous.js
@@ -0,0 +1,20 @@
+// 高階関数を定義する
+function arrayWalk(data, f){
+    for(let key in data){
+        // ここでshowElementを呼び出している
+        f(data[key], key);
+    }
+}
+
+var result = 0
+var arrayNum = [1,10,5,12,3];
+// 値と関数名を引数で与える
+arrayWalk(
+    arrayNum,
+    function (value,key){
+    // 与えられた配列要素で変数resultを加算する
+    result += value;
+});
+console.log(`合計値は${result}`);
+
+
