dc1723f4bef540e2f736b5dc7327e6d78fe7aeab
Fendo181 <fendo181git@gmail.com>
2019-01-27 13:35:45 +0900

    p216 テンプレート文字列をアプリ仕様にカスタマイズする
    タグ付きテンプレート文字列

---
+++ introduction/chap4/tag_tagged.js
@@ -0,0 +1,22 @@
+function escapeHtml(str){
+    if(!str){ return ''; }
+    str = str.replace(/&/g, '&amp');
+    str = str.replace(/</g, '&lt');
+    str = str.replace(/>/g, '&gt');
+    str = str.replace(/"/g, '&quot');
+    str = str.replace(/'/g, '&#39');
+    return str;
+}
+// 分解されたtemplateとvaluesを順に連結させる
+function e(template, ...values){
+    let result = '';
+    let len = template.length;
+    for(let i = 0; i < len; i++){
+        result += template[i] + escapeHtml(values[i]);
+    }
+    return result;
+}
+let name = '<"Mario" & \'Luigi\'>';
+console.log(e`こんにちは！！！${name}`); //こんにちは！！！&lt&quotMario&quot &amp &#39Luigi&#39&gt
+
+
