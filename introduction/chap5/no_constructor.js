3b00477afdaf466c7d18b6471804953634091357
Fendo181 <fendo181git@gmail.com>
2019-02-05 10:16:02 +0900

    コンストラクタ呼び出しされる際に強制的にオブジェクトを呼び出すようにした

--- introduction/chap5/no_constructor.js
+++ introduction/chap5/no_constructor.js
@@ -1,5 +1,8 @@
 var Member = function(firstName, lastName){
     // グローバル変数(this)が生成されてしまう
+    if(!(this instanceof Member)){
+        return new Member(firstName,lastName);
+    }
     this.firstName = firstName;
     this.lastName = lastName;
 };
@@ -7,8 +10,6 @@ var Member = function(firstName, lastName){
 // コンストラクタ呼び出しだけどオブジェクトが生成されない
 var m = Member('菊池','翔太');
 console.log(m)
-console.log(firstName)
-console.log(m.firstName) // Cannot read property 'firstName' of undefined
+// console.log(firstName) //firstName is not defined
+console.log(m.firstName)
 
-var  obj = new Member('菊池','翔太');
-console.log(obj) //Member { firstName: '菊池', lastName: '翔太' }
