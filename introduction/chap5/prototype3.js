d5e957db8132eff5c2359788f62ce3093e2bd1d6
Fendo181 <fendo181git@gmail.com>
2019-02-12 09:33:36 +0900

    p238 プロパティの削除
    インスタンス側でのメンバーの追加や削除と言った操作がプロトタイプオブジェクトまで影響を及ぼす事はない

---
+++ introduction/chap5/prototype3.js
@@ -0,0 +1,21 @@
+// クラス宣言
+var Member = function(){};
+
+Member.prototype.sex = 'Man';
+
+var mem1 = new Member()
+var mem2 = new Member()
+
+console.log(mem1.sex+'|'+mem2.sex);
+mem2.sex = 'Woman'; //mem2は独立している
+console.log(mem1.sex+'|'+mem2.sex);
+
+// インスタンス変数me1のsexプロパティを削除しよとするが、宣言してないので削除されない。
+delete mem1.sex;
+delete mem2.sex;
+// 結果MemberクラスのプロトタイプのMember.prototype.sexまでは削除されないので表示される
+console.log(mem1.sex+'|'+mem2.sex);
+
+
+
+
