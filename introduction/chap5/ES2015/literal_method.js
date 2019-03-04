bc465fbfc14d2f523138575bda6c28fb77fa7165
Fendo181 <fendo181git@gmail.com>
2019-03-04 09:33:19 +0900

    p264 オブジェクトリテラルの改善

---
+++ introduction/chap5/ES2015/literal_method.js
@@ -0,0 +1,12 @@
+//オブジェクトリテラルの改善
+let member = {
+    name: '山田太郎',
+    birth: new Date(1970,5,25),
+    toString() {
+        return this.name + '/誕生日:' + this.birth.toLocaleDateString()
+    }
+};
+
+console.log(member.toString());
+
+//山田太郎/誕生日:1970-6-25
