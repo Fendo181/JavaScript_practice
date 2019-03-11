defbe01d2b63f045ec3306406abb52b7708cd1fc
Fendo181 <fendo181git@gmail.com>
2019-03-11 09:58:18 +0900

    p271 Symbol.iterator]でイテレータオブジェクトを返す

---
+++ introduction/chap5/ES2015/iterator_array.js
@@ -0,0 +1,14 @@
+let data_ary = ['one','two','three']
+let itr = data_ary[Symbol.iterator]();
+let d;
+
+while(d = itr.next()){
+    // イテレータが終端に到達したか(次の要素はないか)
+    if(d.done) {
+        console.log(d.done); //true
+        break;
+    }
+    console.log(d.done);
+    // 次の要素の値
+    console.log(d.value);
+}
