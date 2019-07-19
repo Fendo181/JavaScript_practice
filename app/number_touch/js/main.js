b1498c9baa9e2e5fc699375ebe0a8e914377e49c
Fendo181 <fendo181git@gmail.com>
2019-07-19 10:34:20 +0900

    chap08 JavaScriptでパネルを作成する

--- app/number_touch/js/main.js
+++ app/number_touch/js/main.js
@@ -1,20 +1,38 @@
-'use strict'
+'use strict';
 
 {
+  //  Panelクラス
   class Panel {
     constructor () {
     this.el = document.createElement('li');
-    this.el.classList = classList.add('pressed');
+    this.el.classList.add('pressed');
+    }
+
+    getEl() {
+        return this.el;
     }
   }
+  // Boardクラス
   class Board {
-    constructor (params) {
+    constructor () {
       this.panels = [];
       for (let i = 0; i < 4; i++) {
         this.panels.push(new Panel());
       }
+      this.setup();
+    }
+
+    // パネルの数だけ要素を追加する
+    setup() {
+        const board = document.getElementById('board');
+        this.panels.forEach(panel => {
+            //  クラスのプロパティに外部から直接アクセスしないほうが良い
+            // board.appendChild(panel.el);
+            // getterを使って取得する
+            board.appendChild(panel.getEl());
+        });
     }
   }
 
-  cnst board = new Board;
+  const board = new Board();
 }
