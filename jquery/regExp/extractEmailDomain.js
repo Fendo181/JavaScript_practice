9842bf3c17082f0fa94eee6c5c844bd5b07e29fd
Fendo181 <fendo181git@gmail.com>
2019-09-26 21:47:21 +0900

    無意識にPHPで書いてしまっていたので修正

--- jquery/regExp/extractEmailDomain.js
+++ jquery/regExp/extractEmailDomain.js
@@ -1,12 +1,10 @@
 $('.email').bind('keyup',function(event){
-    $email = event.target.value;
-    $id = $email
+    let email = event.target.value;
+    let id = email
     // 正規表現でメールアドレスのドメイン部分だけを抜き取る
-    if($email.includes('@')){
-        $result = $email.match(/(.+?)@/i);
-        $id = $result[1];
+    if(email.includes('@')){
+        result = email.match(/(.+?)@/i);
+        id = result[1];
     }
-    console.log($id);
-    $('.id').val($id);
+    $('.id').val(id);
 });
-
