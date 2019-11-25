118e88f3bca70f4e36bc4aa44b4d9f7e4cf720e4
Fendo181 <fendo181git@gmail.com>
2019-11-25 19:29:14 +0900

    素のJavaScriptで入力フォームに記述されている内容をコピーするように実装した

---
+++ UserScripts/copy_url/js/myapp.js
@@ -0,0 +1,8 @@
+function copyToClipboard() {
+    var copyClipboard = document.getElementById("copyTarget")
+
+    copyTarget.select()
+    // コピー対処のテキストをクリップボードにコピーする
+    document.execCommand('Copy');
+    console.log("コピーできました!" + copyClipboard.value);    
+}
