0e0fd1a6969c45eb893a0c0078c7dba33801ee06
Fendo181 <fendo181git@gmail.com>
2019-11-25 19:10:40 +0900

    正規表現でメールアドレスのドメイン部分だけを抜き取る

--- jquery/regExp/extractEmailDomain.js
+++ jquery/regExp/extractEmailDomain.js
@@ -3,8 +3,7 @@ $('.email').bind('keyup',function(event){
     let id = email
     // 正規表現でメールアドレスのドメイン部分だけを抜き取る
     if(email.includes('@')){
-        result = email.match(/(.+?)@/i);
-        id = result[1];
+        id = email.match(/(.+?)@/i)[1];
     }
     $('.id').val(id);
 });
