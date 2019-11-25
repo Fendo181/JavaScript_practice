a797e8393cbd247721e972b803a34c4bfb3e8f80
Fendo181 <fendo181git@gmail.com>
2019-11-25 20:09:12 +0900

    spanタグの場合でもクリップボードにテキストをコピーできるようにした

--- UserScripts/copy_url/js/text_box_myapp.js
+++ UserScripts/copy_url/js/text_box_myapp.js
@@ -3,6 +3,22 @@ function copyToTextboxClipboard() {
 
     copyTarget.select()
     // コピー対処のテキストをクリップボードにコピーする
-    document.execCommand('Copy');
+    document.execCommand('copy');
+    console.log("コピーできました!" + copyClipboard.value);    
+}
+
+function copyToTextSpan(){
+    //範囲を指定
+    let range = document.createRange();
+    let span = document.getElementById('copyTargetSpan');
+    range.selectNodeContents(span);
+  
+    //指定した範囲を選択状態にする
+    let selection = document.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+  
+    //コピー
+    document.execCommand('copy');
     console.log("コピーできました!" + copyClipboard.value);    
 }
