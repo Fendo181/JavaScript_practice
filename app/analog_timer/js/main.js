cfbb0da3c558b9a6856482601af5956efc21174f
Fendo181 <fendo181git@gmail.com>
2019-12-03 10:05:03 +0900

    盤面の描画処理を追記した

--- app/analog_timer/js/main.js
+++ app/analog_timer/js/main.js
@@ -31,8 +31,20 @@
         // 線の描画(60個点を盤面に合わせて描画する)
         ctx.beginPath();
         ctx.moveTo(0, -this.r);
+        if (angle % 30 === 0) {
+          // 文字を太くする
+          ctx.lineWidth = 2;
+          ctx.lineTo(0, -this.r + 10);
+          // フォントと位置合わせ
+          ctx.font = '13px arial';
+          // 半径から25下がったところに数字を描画する
+          // 0のところは ||演算子で12が入るようにする
+          ctx.fillText(angle / 30.0 || 12, 0, -this.r + 25);
+        } else {
         // 5px分真下に引く
-        ctx.lineTo(0, -this.r + 5);
+          ctx.lineTo(0, -this.r + 5);
+        }
+
         // 線を描画する
         ctx.stroke();
         ctx.restore();
