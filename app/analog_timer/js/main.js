89d14f30a849a2b4f723dc0ea05e889470a3552d
Fendo181 <fendo181git@gmail.com>
2019-12-03 09:33:59 +0900

    canvasを使ってClockクラスを作成する

--- app/analog_timer/js/main.js
+++ app/analog_timer/js/main.js
@@ -1,1 +1,24 @@
-console.log('Hi!');
+'use strict';
+
+{
+  class Clock {
+    drawFace () {
+      const canvas = document.querySelector('canvas');
+      if (typeof canvas.getContext === 'undefined') {
+        return 0;
+      }
+
+      const ctx = canvas.getContext('2d');
+      // 横幅
+      const width = canvas.width;
+      // 高さ
+      const height = canvas.height;
+    }
+    run () {
+      this.drawFace();
+    }
+  }
+
+  const clock = new Clock();
+  clock.run();
+}
