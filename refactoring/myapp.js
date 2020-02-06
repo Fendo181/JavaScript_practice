0a52d30d9ca73bb6722af7ef98d9853871c72218
Fendo181 <fendo181git@gmail.com>
2020-02-06 10:03:39 +0900

    p10 perfをaPerformanceに変更
    特定の役割を持つ事を名前で伝えたい場合は不定冠詞をつける

--- refactoring/myapp.js
+++ refactoring/myapp.js
@@ -31,23 +31,23 @@ function statement (invoice, plays) {
   return result;
 }
 
-function amountFor (perf, play) {
+function amountFor (aPerfomance, play) {
   let result = 0;
 
   // 演劇のタイプによって請求金額を分けている
   switch (play.type) {
     case 'tragedy' :
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (aPerfomance.audience > 30) {
+        result += 1000 * (aPerfomance.audience - 30);
       }
       break;
     case 'comedy':
       result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (aPerfomance.audience > 20) {
+        result += 10000 + 500 * (aPerfomance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerfomance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
