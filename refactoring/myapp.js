cfa0290360bf18e66c0803d75dbcd805911d4e43
Fendo181 <fendo181git@gmail.com>
2020-02-13 12:38:08 +0900

    p24 renderPlainTextで関数の抽出化を行う

--- refactoring/myapp.js
+++ refactoring/myapp.js
@@ -3,6 +3,11 @@
 const fs = require('fs');
 
 function statement (invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+// 請求書を出力する
+function renderPlainText (invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.perfomances) {
     // 注文の内訳を出力
@@ -11,73 +16,72 @@ function statement (invoice, plays) {
   result += `Amount owed is ${usd(totalAmount(invoice))}\n`;
   result += `Your earned  ${totalVolumeCredits(invoice)} credits \n`;
   return result;
-}
 
-// 演劇のタイプによって請求金額を分けている
-function amountFor (aPerfomance) {
-  let result = 0;
-
-  switch (palyFor(aPerfomance).type) {
-    case 'tragedy' :
-      result = 40000;
-      if (aPerfomance.audience > 30) {
-        result += 1000 * (aPerfomance.audience - 30);
-      }
-      break;
-    case 'comedy':
-      result = 30000;
-      if (aPerfomance.audience > 20) {
-        result += 10000 + 500 * (aPerfomance.audience - 20);
-      }
-      result += 300 * aPerfomance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${palyFor(aPerfomance).type}`);
+  // 演劇のタイプによって請求金額を分けている
+  function amountFor (aPerfomance) {
+    let result = 0;
+    switch (palyFor(aPerfomance).type) {
+      case 'tragedy' :
+        result = 40000;
+        if (aPerfomance.audience > 30) {
+          result += 1000 * (aPerfomance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (aPerfomance.audience > 20) {
+          result += 10000 + 500 * (aPerfomance.audience - 20);
+        }
+        result += 300 * aPerfomance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${palyFor(aPerfomance).type}`);
+    }
+    return result;
   }
-  return result;
-}
 
-function palyFor (aPerfomance) {
-  return plays[aPerfomance.playID];
-}
+  function palyFor (aPerfomance) {
+    return plays[aPerfomance.playID];
+  }
 
-// ポイント計算
-function volumeCreditsFor (aPerfomance) {
-  let result = 0;
-  // ボリューム特典のポイント換算
-  result += Math.max(aPerfomance.audience - 30.0);
-  // comedy は 10人につき、さらにポイント加算
-  if (palyFor(aPerfomance).type === 'comedy') result += Math.floor(aPerfomance.audience / 5);
-  return result;
-}
+  // ポイント計算
+  function volumeCreditsFor (aPerfomance) {
+    let result = 0;
+    // ボリューム特典のポイント換算
+    result += Math.max(aPerfomance.audience - 30.0);
+    // comedy は 10人につき、さらにポイント加算
+    if (palyFor(aPerfomance).type === 'comedy') result += Math.floor(aPerfomance.audience / 5);
+    return result;
+  }
 
-function totalVolumeCredits (invoice) {
-  let volumeCredits = 0;
-  for (let perf of invoice.perfomances) {
+  function totalVolumeCredits (invoice) {
+    let volumeCredits = 0;
+    for (let perf of invoice.perfomances) {
     // ボリューム特典のポイント計算
-    volumeCredits += volumeCreditsFor(perf);
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
   }
-  return volumeCredits;
-}
 
-// formatをUSDにする
-function usd (aNumber) {
-  return new Intl.NumberFormat('en-US',
-    {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2
-    }).format(aNumber / 100);
-}
+  // formatをUSDにする
+  function usd (aNumber) {
+    return new Intl.NumberFormat('en-US',
+      {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+      }).format(aNumber / 100);
+  }
 
-// 請求金額の計算
-function totalAmount (invoice) {
-  let result = 0;
-  for (let perf of invoice.perfomances) {
+  // 請求金額の計算
+  function totalAmount (invoice) {
+    let result = 0;
+    for (let perf of invoice.perfomances) {
     // 請求金額の計算
-    result += amountFor(perf);
+      result += amountFor(perf);
+    }
+    return result;
   }
-  return result;
 }
 
 let invoices = JSON.parse(fs.readFileSync('data/invoices.json'));
