e051eebffd59bfea3ec01d62030e7206cd3e709c
Fendo181 <fendo181git@gmail.com>
2020-03-16 10:01:44 +0900

    コメントアウトさせる

--- study/ECMAScript/basic/js/arrowFunction.js
+++ study/ECMAScript/basic/js/arrowFunction.js
@@ -3,9 +3,9 @@ const sum = (a, b, c) => a + b + c;
 console.log(`${sum(1, 2, 3)}`);
 
 // 関数式
-const double = function (a) {
-  return a * 2;
-};
+// const double = function (a) {
+//   return a * 2;
+// };
 
 // アロー関数
 
