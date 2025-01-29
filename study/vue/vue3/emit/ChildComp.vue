74b6767b28d3552c9c47dbe36e9aaade92dc328c
Fendo181 <fendo181git@gmail.com>
2025-01-29 16:36:41 +0900

    feat: emitについて学んだ

---
+++ study/vue/vue3/emit/ChildComp.vue
@@ -0,0 +1,12 @@
+<script setup>
+// 発行されるイベントを宣言
+const emit = defineEmits(['response'])
+
+// responseイベントに対して、hello from childを渡している
+emit('response', 'hello from child')
+</script>
+
+<template>
+  <h2>Child component</h2>
+  <h3>そうだね!</h3>
+</template>
