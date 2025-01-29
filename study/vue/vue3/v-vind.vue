adab4824da6aea94448ae838cf3d612363423bbf
Fendo181 <fendo181git@gmail.com>
2025-01-29 14:46:30 +0900

    v-bindの使い方を学んだ

---
+++ study/vue/vue3/v-vind.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const titleClass = ref('title')
+</script>
+
+<template>
+  <h1 v-bind:class="titleClass">Make me red</h1>
+	<!-- これでも書ける   -->
+  <h1 :class="titleClass">どうも！！</h1>
+</template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
