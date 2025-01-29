b434db1eca0f3ebb8370a3592ed6ba3e4b84ae73
Fendo181 <fendo181git@gmail.com>
2025-01-29 14:49:20 +0900

    feat: v-onの使い方について学ぶ

---
+++ study/vue/vue3/v-on.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <!-- make this button work -->
+  <button>Count is: {{ count }}</button>
+  <button v-on:click='increment'>
+    Count is {{ count }}
+  </button>
+  <!-- v-onは@で省略できます   -->
+    <button @click='increment'>
+    Count is {{ count }}
+  </button>
+</template>
